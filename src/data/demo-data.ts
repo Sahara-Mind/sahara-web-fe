06a6e57559a864b6d081ad1dffdfe026288693f4
Pranish Lama <pranishlama17@gmail.com>
2025-06-26 15:38:02 +0545

    feat: add demo data for users

---
+++ src/data/demo-data.ts
@@ -0,0 +1,29 @@
+export const DemoData = [
+    {
+        id: '1',
+        name: 'Pranish Lama',
+        avatar: 'https://static.wikia.nocookie.net/naruto/images/b/bb/Itachi.png/revision/latest/scale-to-width-down/300?cb=20220214112531',
+        email: 'pranishlama17@gmail.com',
+        specialization: 'Anxiety Trauma',
+        phone: 984073333,
+        status: 'active',
+    },
+    {
+        id: '2',
+        name: 'Yugal Pariyar',
+        avatar: 'https://static.wikia.nocookie.net/naruto/images/b/bb/Itachi.png/revision/latest/scale-to-width-down/300?cb=20220214112531',
+        email: 'yugal@gmail.com',
+        specialization: 'Depression',
+        phone: 984073333,
+        status: 'active',
+    },
+    {
+        id: '3',
+        name: 'Nabin Lama',
+        avatar: 'https://static.wikia.nocookie.net/naruto/images/b/bb/Itachi.png/revision/latest/scale-to-width-down/300?cb=20220214112531',
+        email: 'nabin@gmail.com',
+        specialization: 'Family Therapy',
+        phone: 984073333,
+        status: 'active',
+    }
+]
