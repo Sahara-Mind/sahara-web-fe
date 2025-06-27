d6af9078b3bd769c6bbd827589cd2f0055cdabbf
Pranish Lama <pranishlama17@gmail.com>
2025-06-27 17:19:06 +0545

    feat: added therapist list page

--- src/app/dashboard/@admin/page.tsx
+++ src/app/dashboard/@admin/page.tsx
@@ -1,11 +1,10 @@
 import Link from "next/link"
 
-export default async function AdminDashboard({ children }: { children: React.ReactNode }) {
+export default async function AdminDashboard() {
   return (
     <div>
       <h1>Admin Dashboard</h1>
       <Link href="/dashboard/therapist-list">Therapist List</Link>
-      <div>{children}</div>
     </div>
   );
 }
--- src/app/dashboard/@admin/therapist-list/page.tsx
+++ src/app/dashboard/@admin/therapist-list/page.tsx
@@ -1,3 +1,43 @@
-export default function TherapistList(){
-    return <h1>Therapist lists</h1>
+import { Box } from "rizzui/box";
+import { routes } from "@/config/routes";
+import { DemoData } from "@/data/demo-data";
+import { metaObject } from "@/config/site.config";
+import TableLayout from "@/app/shared/tables/table-layout";
+import RetroTable from "@/app/shared/tables/retro-table";
+import WidgetCard from "@/components/cards/widget-card";
+
+export const metadata = {
+  ...metaObject("Basic Table"),
+};
+
+const pageHeader = {
+  title: "Therapists List",
+  breadcrumb: [
+    {
+      href: routes.therapist,
+      name: "Dashboard",
+    },
+    {
+      name: "Therapists",
+    },
+  ],
+};
+
+
+export default function BasicTablePage() {
+  return (
+    <TableLayout
+      title={pageHeader.title}
+      breadcrumb={pageHeader.breadcrumb}
+      data={DemoData}
+      fileName="therapist_data"
+      header= "ID, Name, Specialization, Email, Phone, Status, Actions" 
+    >
+      <Box className="space-y-8">
+        <WidgetCard title="Therapists" className="space-y-4">
+          <RetroTable />
+        </WidgetCard>
+      </Box>
+    </TableLayout>
+  );
 }
